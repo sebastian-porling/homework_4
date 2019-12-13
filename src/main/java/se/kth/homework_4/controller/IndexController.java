6af4610db4a79e37c9c0873a62125bc636d9d7e5
sebastian-porling <sebastian@n128-p206.eduroam.kth.se>
2019-12-13 13:17:42 +0100

    Some final changes to the styles

--- src/main/java/se/kth/homework_4/controller/IndexController.java
+++ src/main/java/se/kth/homework_4/controller/IndexController.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,14 +23,27 @@ class IndexController implements ErrorController {
         int statusCode = Integer.parseInt(extractHttpStatusCode(request));
         System.err.println("ERROR: " + statusCode);
         if (statusCode == HttpStatus.NOT_FOUND.value()){
-
+            System.err.println("***** PAGE NOT FOUND ******");
+            model.addAttribute("errorType", "404 page not found");
+            model.addAttribute("errorMessage", "The page you tried to reach doesn't exist on this server.");
         } else if (statusCode == HttpStatus.BAD_REQUEST.value()){
-            System.out.println();
+            System.err.println("***** BAD REQUEST ******");
+            model.addAttribute("errorType", "500 Bad reqeust");
+            model.addAttribute("errorMessage", "The action you tried to make is not possible.");
         } else {
             System.out.println(HttpStatus.valueOf(statusCode).toString());
         }
         model.addAttribute("error", "Not valid input");
-        return "redirect:/";
+        return "error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception exc, Model model) {
+        System.err.println("***** INTERNAL ERROR ******");
+        model.addAttribute("errorType", "INTERNAL SERVER ERROR");
+        model.addAttribute("errorMessage", "The action you tried to make is not possible. Please try again.");
+        return "error";
     }
 
     private String extractHttpStatusCode(HttpServletRequest request) {
